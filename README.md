1bf5408aa846fb916898e076c44665e80ebe9530
Steve Harmeyer <harmeyersteve@gmail.com>
2019-09-16 14:04:07 -0700

    initial commit + move from dbdnet/provable to gamebetr/provable

---
+++ README.md
@@ -0,0 +1,103 @@
+# Provable
+
+This package provides the means to create provibly fair random numbers and provably fair random shuffles.
+
+## Installation
+
+via composer:
+```
+composer require dbdnet/provable ^1.0
+```
+
+## Basic Useage
+
+```php
+// set some vars
+$clientSeed = 'your client seed here';
+$serverSeed = 'your server seed here';
+$min = 1;
+$max = 52;
+$type = 'shuffle';
+
+// instanciate the provable class
+$provable = new DBD\Provable\Provable($clientSeed, $serverSeed, $min, $max, $type);
+
+// get the results
+print $provable->results();
+// prints [6,21,19,13,41,28,40,43,2,39,5,24,18,52,46,26,20,7,29,38,23,37,30,31,33,44,22,16,35,48,25,14,45,27,11,8,17,36,51,4,42,15,49,32,3,9,1,47,10,34,50,12]
+```
+
+## Methods
+
+### __construct(string $clientSeed = null, string $serverSeed = null, int $min = 0, int $max = 0, string $type = 'number')
+
+The class constructor takes the optional parameters, clientSeed, serverSeed, min, max, and type. If clientSeed or serverSeed are not provided, it will generate random seeds automatically. The min and max parameters are the minimum and maximum values of the random number or shuffle. Type is either `number` or `shuffle`.
+
+### static init(string $clientSeed = null, string $serverSeed = null, int $min = 0, int $max = 0, string $type = 'number')
+
+The init method is just a static constructor. It allows you to do the following:
+
+```
+$provable = DBD\Provable::init()
+// returns an instance of DBD\Provable
+```
+
+### setClientSeed(string $clientSeed = null)
+
+This sets the client seed. If no seed is provided, one will be automatically generated. The Provable instance is returned allowing you to chain commands.
+
+### getClientSeed()
+
+This returns the current client seed.
+
+### setServerSeed(string $serverSeed = null)
+
+This sets the server seed. If no seed is provided, one will be automatically generated. The Provable instance is returned allowing you to chain commands.
+
+### getServerSeed()
+
+This returns the current server seed.
+
+### getHashedServerSeed()
+
+This returns the hashed version of the server seed.
+
+### setMin(int $min)
+
+This sets the minimum value property. The Provable instance is returned allowing you to chain commands.
+
+### getMin()
+
+This returns the current minimum value property.
+
+### setMax(int $max)
+
+This sets the maximum value property. The Provable instance is returned allowing you to chain commands.
+
+### getMax()
+
+This returns the current maximum value property.
+
+### setType(string $type)
+
+This sets the type property. Allowed values are number and shuffle. The Provable instance is returned allowing you to chain commands.
+
+### getType()
+
+This returns the current type property.
+
+### results()
+
+This calculates the random number or shuffle and returns it.
+
+### number(int $minimumNumber = null, int $maximumNumber = null)
+
+This generates a random number between $minimumNumber and $maximumNumber. If no values are provided, it will use the $min and $max properties of the object.
+
+### shuffle(int $minimumNumber = null, int $maximumNumber = null)
+
+This generates a random shuffle of numbers between $minimumNumber and $maximumNumber. If no values are provided, it will use the $min and $max properties of the object.
+
+
+
+
---
+++ src/Provable.php
@@ -0,0 +1,255 @@
+<?php
+
+namespace DBD\Provable;
+
+class Provable
+{
+    /**
+     * client seed
+     * @var string $clientSeed
+     */
+    private $clientSeed;
+
+    /**
+     * server seed
+     * @var string $serverSeed
+     */
+    private $serverSeed;
+
+    /**
+     * min number
+     * @var int $min
+     */
+    private $min;
+
+    /**
+     * max number
+     * @var int $max
+     */
+    private $max;
+
+    /**
+     * type
+     * @var string $type
+     */
+    private $type;
+
+    /**
+     * class constructor
+     * @param string $clientSeed
+     * @param string $serverSeed
+     * @param int $min
+     * @param int $max
+     * @param string $type
+     */
+    public function __construct(string $clientSeed = null, string $serverSeed = null, int $min = 0, int $max = 0, string $type = 'number')
+    {
+        $this->setClientSeed($clientSeed);
+        $this->setServerSeed($serverSeed);
+        $this->setMin($min);
+        $this->setMax($max);
+        $this->setType($type);
+    }
+
+    /**
+     * static constructor
+     * @param string $clientSeed
+     * @param string $serverSeed
+     * @param int $min
+     * @param int $max
+     * @param string $type
+     * @return Provable
+     */
+    public static function init(string $clientSeed = null, string $serverSeed = null, int $min = 0, int $max = 0, string $type = 'number')
+    {
+        return new static($clientSeed, $serverSeed, $min, $max, $type);
+    }
+
+    /**
+     * client seed setter
+     * @param string $clientSeed
+     * @return Provable
+     */
+    public function setClientSeed(string $clientSeed = null)
+    {
+        $this->clientSeed = $clientSeed ?? $this->generateRandomSeed();
+        return $this;
+    }
+
+    /**
+     * client seed getter
+     * @return string
+     */
+    public function getClientSeed()
+    {
+        return $this->clientSeed;
+    }
+
+    /**
+     * server seed setter
+     * @param string $serverSeed
+     * @return Provable
+     */
+    public function setServerSeed(string $serverSeed = null)
+    {
+        $this->serverSeed = $serverSeed ?? $this->generateRandomSeed();
+        return $this;
+    }
+
+    /**
+     * server seed getter
+     * @return string
+     */
+    public function getServerSeed()
+    {
+        return $this->serverSeed;
+    }
+
+    /**
+     * hashed server seed getter
+     * @return string
+     */
+    public function getHashedServerSeed()
+    {
+        return hash('sha256', $this->getServerSeed());
+    }
+
+    /**
+     * min setter
+     * @param int $min
+     * @return Provable
+     */
+    public function setMin(int $min)
+    {
+        $this->min = $min;
+        return $this;
+    }
+
+    /**
+     * min getter
+     * @return int
+     */
+    public function getMin()
+    {
+        return $this->min;
+    }
+
+    /**
+     * max setter
+     * @param int $max
+     * @return Provable
+     */
+    public function setMax(int $max)
+    {
+        $this->max = $max;
+        return $this;
+    }
+
+    /**
+     * max getter
+     * @return int
+     */
+    public function getMax()
+    {
+        return $this->max;
+    }
+
+    /**
+     * type setter
+     * @param string $type - number|shuffle
+     * @return Provable
+     */
+    public function setType(string $type)
+    {
+        if (!in_array($type, ['number', 'shuffle'])) {
+            throw new \Exception("Invalid type $type", 400);
+        }
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * type getter
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * returns the results
+     * @return int|array
+     */
+    public function results() {
+        if($this->getType() == 'number') {
+            return $this->number();
+        }
+        if($this->getType() == 'shuffle') {
+            return $this->shuffle();
+        }
+    }
+
+    /**
+     * returns a random number within a range
+     * @param int $minimumNumber
+     * @param int $maximumNumber
+     * @return Int
+     */
+    public function number(int $minimumNumber = null, int $maximumNumber = null)
+    {
+        if ($minimumNumber !== null) {
+            $this->setMin($minimumNumber);
+        }
+        if ($maximumNumber !== null) {
+            $this->setMax($maximumNumber);
+        }
+        mt_srand($this->generateSeedInteger());
+        return mt_rand($this->getMin(), $this->getMax());
+    }
+
+    /**
+     * returns a random shuffle of numbers within a range
+     * uses fisher yates shuffle (https://en.wikipedia.org/wiki/Fisher–Yates_shuffle)
+     * @param int $minimumNumber
+     * @param int $maximumNumber
+     * @return array
+     */
+    public function shuffle(int $minimumNumber = null, int $maximumNumber = null)
+    {
+        if ($minimumNumber !== null) {
+            $this->setMin($minimumNumber);
+        }
+        if ($maximumNumber !== null) {
+            $this->setMax($maximumNumber);
+        }
+        $range = range($this->getMin(), $this->getMax());
+        mt_srand($this->generateSeedInteger());
+        for ($i = count($range) - 1; $i > 0; $i--) {
+            $j = mt_rand(0, $i);
+            $tmp = $range[$i];
+            $range[$i] = $range[$j];
+            $range[$j] = $tmp;
+        }
+        return $range;
+    }
+
+    /**
+     * generate a seed integer from server seed and client seed
+     * @return int
+     */
+    private function generateSeedInteger()
+    {
+        return hexdec(substr(hash_hmac('sha256', $this->getServerSeed(), $this->getClientSeed()), -8, 8));
+    }
+
+    /**
+     * generate a random seed
+     * @var int $seedLength
+     * @return string
+     */
+    private function generateRandomSeed()
+    {
+        return bin2hex(openssl_random_pseudo_bytes(32));
+    }
+}
